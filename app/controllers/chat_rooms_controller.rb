a6677d8ae1bb9ac5c4df4d4127e2327f4c52b43a
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-11-02 18:45:56 -0400

    create only if it doesn't already exist

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -8,10 +8,12 @@ class ChatRoomsController < ApplicationController
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
     @message = Message.new
+
     if @chat_room.user_id == current_user.id || @chat_room.username == current_user.username
       @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
       @message = Message.new
     else
+      flash[:alert] = "Whoops! You don't belong in there!"
       redirect_to chat_rooms_path
     end
   end
@@ -21,14 +23,22 @@ class ChatRoomsController < ApplicationController
   end
 
   def create
+    @chat_rooms = ChatRoom.all
+
+    @chat_rooms.each do |cr|
+      if cr.user_id == current_user.id || cr.username == current_user.username
+        redirect_to chat_rooms_path and return
+      end
+    end
+
     @chat_room = ChatRoom.new(chat_room_params)
     @chat_room.user_id = current_user.id
 
     if @chat_room.save
       flash[:success] = 'Chat room added!'
-      redirect_to chat_rooms_path
+      redirect_to chat_room_path(@chat_room)
     else
-      render 'new'
+      redirect_to chat_rooms_path
     end
   end
 
