03db0e600eff36c3ebdde667529e4a7792f1e126
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-11-03 22:04:44 -0400

    chats create functioning

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,14 +2,14 @@ class ChatRoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @chat_rooms = ChatRoom.where('user_id=? OR username=?', current_user.id, current_user.username)
+    @chat_rooms = current_user.chat_rooms
   end
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
     @message = Message.new
 
-    if @chat_room.user_id == current_user.id || @chat_room.username == current_user.username
+    if @chat_room.users.includes(current_user)
       @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
       @message = Message.new
     else
@@ -20,15 +20,16 @@ class ChatRoomsController < ApplicationController
 
   def create
     @chat_rooms = ChatRoom.all
+    @other_user = params[:chat_room][:user]
 
     @chat_rooms.each do |cr|
-      if cr.user_id == current_user.id && cr.username == @user.username
-        redirect_to chat_rooms_path and return
+      if cr.users.includes(current_user)  && cr.users.includes(@other_user)
+        redirect_to chat_room_path(cr) and return
       end
     end
 
     @chat_room = ChatRoom.new(chat_room_params)
-    @chat_room.user_id = current_user.id
+    @chat_room.users << current_user
 
     if @chat_room.save
       flash[:success] = 'Chat room added!'
@@ -41,6 +42,6 @@ class ChatRoomsController < ApplicationController
   private
 
   def chat_room_params
-    params.require(:chat_room).permit(:title, :username)
+    params.require(:chat_room).permit(:title, :users)
   end
 end
