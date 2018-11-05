47789f01bf60db6cc328ccd91c95e2e2f70ae8ab
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-11-05 16:59:02 -0500

    views updated

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,4 +2,6 @@ class ChatRoom < ApplicationRecord
   has_many :chat_room_users, dependent: :destroy
   has_many :users, through: :chat_room_users
   has_many :messages, dependent: :destroy
+
+  default_scope { order('created_at DESC') }
 end
