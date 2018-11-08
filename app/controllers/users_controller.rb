6d951f84967e92badbe2d07a707faca51bb824bf
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-11-08 17:11:15 -0500

    index view updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = User.where.not(id: current_user.id)
+    @favorites = current_user.favorited_users
+    @chat_room = ChatRoom.new
   end
 
   def show
