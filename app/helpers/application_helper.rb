0b9dc62fa4a9e5ec556bc801c4dafd5dfcf7757b
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-11-14 16:23:25 -0500

    only show search bar on desktop and tablets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{opts.delete(:size) { 40 }}",
               opts
   end
+
+  def mobile_device
+    agent = request.user_agent
+    return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
+    return "mobile" if agent =~ /Mobile/
+    return "desktop"
+  end
 end
