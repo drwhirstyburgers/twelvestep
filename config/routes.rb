238e3e2bdc07240882b54739bfdcbe956d4e1c0f
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-11-22 14:44:56 -0500

    failure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'notifications/index'
+
   devise_for :users
 
   resources :users, only: [:index, :show, :create] do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120193712) do
+ActiveRecord::Schema.define(version: 20181122193034) do
 
   create_table "chat_room_users", force: :cascade do |t|
     t.integer "user_id"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20181120193712) do
     t.datetime "updated_at", null: false
     t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "event"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "terms", force: :cascade do |t|
